1634ca79eda956f65f9327fd497c0152dc3a814e
Jack <siwei@chinawebeye.com>
2017-08-23 21:48:07 +0800

    test

--- app/src/main/java/com/example/notepad/NoteList.java
+++ app/src/main/java/com/example/notepad/NoteList.java
@@ -71,6 +71,7 @@ public class NoteList extends AppCompatActivity {
                 }
             }
         });
+        
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setAdapter(mMyAdapter);
